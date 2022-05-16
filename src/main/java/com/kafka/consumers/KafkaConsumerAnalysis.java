0748a0ee51f25b144c48eedfaff4b60d1bdf86b4
zhouguangping <1445101933@qq.com>
2022-05-16 23:22:23 +0800

    🦋序列化器学习

---
+++ src/main/java/com/kafka/consumers/KafkaConsumerAnalysis.java
@@ -0,0 +1,55 @@
+package com.kafka.consumers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Slf4j
+public class KafkaConsumerAnalysis {
+    public static final String brokerList = "localhost:9092,localhost:9093,localhost:9094";
+    public static final String topic = "kinaction_helloworld";
+    public static final String groupId = "group.demo";
+    public static final AtomicBoolean isRunning = new AtomicBoolean(true);
+
+    public static Properties initConfig(){
+        Properties props = new Properties();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer.client.id.demo");
+        return props;
+    }
+
+    public static void main(String[] args) {
+        Properties props = initConfig();
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topic));
+
+        try {
+            while (isRunning.get()) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("topic = " + record.topic()
+                            + ", partition = "+ record.partition()
+                            + ", offset = " + record.offset());
+                    System.out.println("key = " + record.key()
+                            + ", value = " + record.value());
+                    //do something to process record.
+                }
+            }
+        } catch (Exception e) {
+            log.error("occur exception ", e);
+        } finally {
+            consumer.close();
+        }
+    }
+}
---
+++ src/main/java/com/kafka/serializer/MessageDtoSerializer.java
@@ -0,0 +1,36 @@
+package com.kafka.serializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafka.entity.MessageDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+import java.util.Map;
+
+@Slf4j
+public class MessageDtoSerializer implements Serializer<MessageDto> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+
+    @Override
+    public byte[] serialize(String topic, MessageDto data) {
+        try {
+            if (data == null){
+                System.out.println("Null received at serializing");
+                return null;
+            }
+            System.out.println("Serializing...");
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error when serializing MessageDto to byte[]");
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+}
